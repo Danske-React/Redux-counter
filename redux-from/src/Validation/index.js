6c27a31b2195dd539f5865abf33c003aa922155e
nishant Gupta <kr.nish99@gmail.com>
2022-03-24 18:15:31 +0530

    inline validations

--- redux-from/src/Validation/index.js
+++ redux-from/src/Validation/index.js
@@ -1,13 +1,44 @@
-export const validate = inputs => {
-    const errors = {} ;
-    if (!inputs.username) {
-        errors.username = "Enter your username";
-    }
-    else if (inputs.username !=='nishant') {
-        errors.username = "User name is incorrect"
-    }
-    if (!inputs.password) {
-        errors.password = "Enter your password"
-    }
-    return errors;
-};
+export const requiredInput = (input) => 
+input ? undefined : `Input is required`;
+
+export const correctInput = input => 
+input !=='nishant' ? 'incorrect username' : undefined;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const validate = inputs => {
+//     const errors = {} ;
+//     if (!inputs.username) {
+//         errors.username = "Enter your username";
+//     }
+//     else if (inputs.username !=='nishant') {
+//         errors.username = "User name is incorrect"
+//     }
+//     if (!inputs.password) {
+//         errors.password = "Enter your password"
+//     }
+//     return errors;
+// };
--- redux-from/src/components/LoginForm/index.js
+++ redux-from/src/components/LoginForm/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 import { myInput } from "../Field";
-import {validate}  from "../../Validation"
+import {requiredInput, correctInput}  from "../../Validation"
 class LoginForm extends React.Component {
     render() {
         const { handleSubmit } = this.props;
         return (
             <form onSubmit={handleSubmit}>
-                <Field name="username" component={myInput} type="text" placeholder="Username" />
-                <Field name="password" component={myInput} type="password" placeholder="Password" />
+                <Field name="username" component={myInput} type="text" placeholder="Username" validate = {[requiredInput, correctInput]} />
+                <Field name="password" component={myInput} type="password" placeholder="Password" validate={[requiredInput]} />
                 {/* {
                     meta.error && meta.touched &&
                     <div>
@@ -23,7 +23,8 @@ class LoginForm extends React.Component {
 // Field props -> name: acts as identifier , component -> it is a prop that refers to the html element of that field.
 LoginForm = reduxForm({
     form: 'login',
-    validate
+    
+    
 })(LoginForm); // this syntax you have to remember 
 
 export default LoginForm
