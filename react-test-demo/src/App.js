d54b7ed949fe09dbad270ed9a5cb009e4556fed7
nishant Gupta <kr.nish99@gmail.com>
2022-03-25 16:12:33 +0530

    testing prop types using prop types and check prop types module

--- react-test-demo/src/App.js
+++ react-test-demo/src/App.js
@@ -3,12 +3,21 @@ import Header from "./components/header";
 import './app.scss'
 import Headline from './components/headline'
 
+
+const tempArr = [{
+  fName: "Nishant",
+  lName: "Gupta",
+  email: "nishant@email.com",
+  age: 32,
+  onlineStatus: true
+}]
+
 function App() {
   return (
     <div >
      <Header/>
      <section className="main">
-       <Headline header="POSTS" desc="Click the button to render posts"/>
+       <Headline header="Posts" desc="Click the button to render posts" tempArr={tempArr}/>
 
      </section>
     </div>
--- react-test-demo/src/components/headline/spec.test.js
+++ react-test-demo/src/components/headline/spec.test.js
@@ -2,12 +2,35 @@ import React from "react";
 import { shallow } from "enzyme";
 import Headline from "./index";
 import { findByTestAttr } from "../../../Utils";
+import checkPropTypes from "check-prop-types";
+
 
 const setUp = (props = {}) => {
     const component = shallow(<Headline {...props} />);
     return component
 }
 describe('Headline Component', () => {
+
+    describe('Checking our proptypes', () => {
+        it('I should not throw a warning', () => {
+            const expectedProps = {
+                header: "test Header",
+                desc: "test desc ",
+                tempArr: [{
+                    fName: "test name ",
+                    lName: "test lname",
+                    email: "test@email.com",
+                    age: 12,
+                    onlineStatus: false
+                }]
+            };
+            const propsErr = checkPropTypes(Headline.propTypes, expectedProps, "props", Headline.name);
+            expect(propsErr).toBeUndefined()
+
+        })
+    })
+
+
 
     describe('have props ', () => {
         let wrapper;
@@ -22,16 +45,17 @@ describe('Headline Component', () => {
             const component = findByTestAttr(wrapper, 'HeadlineComponent')
             expect(component.length).toBe(1)
         });
-        it('Should be in a H1 tags', () =>{
+        it('Should be in a H1 tags', () => {
             const h1 = findByTestAttr(wrapper, 'header')
             expect(h1.length).toBe(1)
-        }) 
+        })
         it('Should render a desc ', () => {
             const desc = findByTestAttr(wrapper, 'desc')
             expect(desc.length).toBe(1)
-
         })
     });
+
+
     describe('have no props ', () => {
         let wrapper;
         beforeEach(() => {
